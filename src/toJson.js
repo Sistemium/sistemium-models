b50a6e497d0122e33389211d07d1ee035716f3fc
Alexander Levin <alevin.ru@gmail.com>
2020-01-02 18:05:16 +0200

    Create toJson.js

---
+++ src/toJson.js
@@ -0,0 +1,55 @@
+import convert from 'xml-js';
+import lo from 'lodash';
+import fs from 'fs';
+
+convertModel('iSisDriver');
+convertModel('iSisSales');
+
+function convertModel(name) {
+
+  const xml = fs.readFileSync(`./models/${name}.xcdatamodel/contents`, 'utf8');
+
+  const options = {
+    ignoreComment: true,
+    alwaysArray: ['attribute', 'entity', 'relationship'],
+    compact: true,
+    ignoreDeclaration: true,
+  };
+
+  const parsed = convert.xml2js(xml, options);
+
+  const { model: { _attributes: attributes, entity } } = parsed;
+
+  const result = {
+    model: {
+      ...attributes,
+      entity: mapEntity(entity),
+    },
+  };
+
+  console.log(result.model.entity[0]);
+
+  fs.writeFileSync(`./dist/${name}.json`, JSON.stringify(result, null, 2));
+
+}
+
+function mapEntity(entities) {
+  return lo.map(entities, ({ relationship, attribute, _attributes }) => {
+    const res = {
+      ..._attributes,
+      attribute: lo.map(attribute, objAttributes),
+      relationship: lo.map(relationship, objAttributes),
+    };
+    if (!res.attribute.length) {
+      delete res.attribute;
+    }
+    if (!res.relationship.length) {
+      delete res.relationship;
+    }
+    return res;
+  });
+}
+
+function objAttributes(obj) {
+  return lo.omit({ ...obj, ...obj._attributes }, ['_attributes']);
+}
