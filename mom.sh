6000ce6178cc0e63d923e0af0a0828a226025499
Alexander Levin <alevin.ru@gmail.com>
2020-10-31 13:44:39 +0200

    Update mom.sh

--- mom.sh
+++ mom.sh
@@ -3,7 +3,9 @@
 DIST=$(pwd)/dist
 echo $DIST
 
-xcrun momc --no-warnings ./models/iSisSales.xcdatamodel $DIST/iSisSales.mom
-xcrun momc --no-warnings ./models/iSisDriver.xcdatamodel $DIST/iSisDriver.mom
-xcrun momc --no-warnings ./models/iSisWarehouse.xcdatamodel $DIST/iSisWarehouse.mom
-xcrun momc --no-warnings ./models/vfs.xcdatamodel $DIST/vfs.mom
+SRC=$(pwd)/models
+
+xcrun momc --no-warnings $SRC/iSisSales.xcdatamodel $DIST/iSisSales.mom
+xcrun momc --no-warnings $SRC/iSisDriver.xcdatamodel $DIST/iSisDriver.mom
+xcrun momc --no-warnings $SRC/iSisWarehouse.xcdatamodel $DIST/iSisWarehouse.mom
+xcrun momc --no-warnings $SRC/vfs.xcdatamodel $DIST/vfs.mom
